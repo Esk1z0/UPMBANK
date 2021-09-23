b37068d7f3d465ec7ad4e4517335c62e5c5ceb42
Eskizo2033 <j.esteban.rincon.m@gmail.com>
2021-09-23 10:20:03 +0200

    Añadí el sign in

---
+++ src/Principal.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido, ¿qué desea?");
+        System.out.print("Entrar (1) ");
+        System.out.print("Darse de Alta (2)");
+        Scanner entrada = new Scanner(System.in);
+
+    }
+}
---
+++ src/Sign_In.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Sign_In {
+    public static void main(String[] args) {
+
+        String nombre, apellidos;
+        char letraDNI;
+        int cuenta, fechaNac;
+        long IBAN, DNI;
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Para crear una cuenta en UPMBank se le va a pedir una serie de datos");
+        System.out.print("Nombre: ");
+
+    }
+}
