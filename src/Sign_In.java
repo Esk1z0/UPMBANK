c5a99f461395c43d8eff82e8778dbed3c0100dab
Eskizo2033 <j.esteban.rincon.m@gmail.com>
2021-10-07 11:16:16 +0200

    he quitado el switch del dni y añadido el email

--- src/Sign_In.java
+++ src/Sign_In.java
@@ -3,12 +3,9 @@ import java.util.Scanner;
 public class Sign_In {
     public static void main(String[] args) {
 
-        String nombre, apellidos;
-        String letraDNI;
-        int tipoCuenta, fechaNac, diaNac, mesNac, añoNac, pruebaDNI;
-        long IBAN, numDNI;
-
         //Aqui pedimos el nombre al maromo
+
+        String nombre, apellidos;
 
         Scanner entrada = new Scanner(System.in);
         System.out.println("Para crear una cuenta en UPMBank se le va a pedir una serie de datos");
@@ -19,6 +16,8 @@ public class Sign_In {
 
         //Aquí pedimos la fecha de nacimiento
 
+        int diaNac, mesNac, añoNac;
+
         System.out.print("\nDía de nacimiento (dd): ");
         diaNac = entrada.nextInt();
         System.out.print("\nMes de nacimiento (mm): ");
@@ -26,8 +25,11 @@ public class Sign_In {
         System.out.print("\nAño de nacimiento (aa): ");
         añoNac = entrada.nextInt();
         System.out.printf("\nSu fecha de nacimiento es: %02d/%02d/%02d", diaNac, mesNac, añoNac);
-
         //Aquí pedimos el dni y comprobamos que es verdadera
+
+        String letradni;
+        int tipoCuenta, pruebaDNI;
+        long numDNI;
 
         System.out.print("\nDeme los números de su DNI: ");
         numDNI = entrada.nextLong();
@@ -36,175 +38,25 @@ public class Sign_In {
         else
             System.out.println("Número aceptado");
         System.out.print("\nAhora introduzca la letra de su DNI: ");
-        letraDNI = entrada.next();
+        letradni = entrada.next();
         pruebaDNI = (int)(numDNI % 23);
-        switch (pruebaDNI){
-            case 0:
-                if (letraDNI == "T"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 1:
-                if (letraDNI == "R"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 2:
-                if (letraDNI == "W"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 3:
-                if (letraDNI == "A"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 4:
-                if (letraDNI == "G"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 5:
-                if (letraDNI == "M"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 6:
-                if (letraDNI == "Y"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 7:
-                if (letraDNI == "F"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 8:
-                if (letraDNI == "P"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 9:
-                if (letraDNI == "D"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 10:
-                if (letraDNI == "X"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 11:
-                if (letraDNI == "B"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 12:
-                if (letraDNI == "N"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 13:
-                if (letraDNI == "J"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 14:
-                if (letraDNI == "Z"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 15:
-                if (letraDNI == "S"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 16:
-                if (letraDNI == "Q"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 17:
-                if (letraDNI == "V"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 18:
-                if (letraDNI == "H"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 19:
-                if (letraDNI == "L"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 20:
-                if (letraDNI == "C"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 21:
-                if (letraDNI == "K"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            case 22:
-                if (letraDNI == "E"){
-                    System.out.println("Su DNI es correcto: " + numDNI + letraDNI);
-                }
-                else {
-                    System.out.println("Su DNI no es correcto");
-                }
-            default:
 
-        }
+        //Ahora se valida el correo electronico
+
+        String correo = "a";
+
+        do{
+            System.out.println("\nIntroduzca un correo válido de la UPM:");
+            correo = entrada.nextLine();
+        }while (!correo.contains("@alumnos.upm.es") && !correo.contains("@upm.es"));
 
 
+        System.out.print("\nSu cuenta ya está regitrada. Felicidades");
+
+        //Ahora se escoge el tipo de cuenta
+
+        System.out.println("\nIntroduzca que tipo de cuenta quiere\n");
 
     }
+
 }
