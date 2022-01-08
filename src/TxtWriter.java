c2414256a05e03541161686d7d75df7e2539cff5
Esk1z0 <j.esteban.rincon.m@gmail.com>
2022-01-08 22:00:02 +0100

    Ni puta idea que he hecho

--- src/TxtWriter.java
+++ src/TxtWriter.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class TxtWriter {
     public static void matrizTransferencia(Cuenta[] listaCuentas, Operacion listaOperaciones){
